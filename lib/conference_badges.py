b94ba73f670816b0261f210f32c584221539f37c
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-28 16:22:14 +0300

    completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+     return f"Hello, my name is {name}."
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(names): 
+    badges=[]     
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms=[]   
+    for index,name in enumerate(names):
+        room_number=index+1
+        rooms.append(f"Hello, {name}! You'll be assigned to room {index+ 1}!")
+    return rooms
 
 def printer(names):
-    return None
+    badges=batch_badge_creator(names)
+    rooms=assign_rooms(names)
+    for badge in badges:
+        print(badge)
+
+    for room in rooms:
+        print(room)
